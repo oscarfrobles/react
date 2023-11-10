dddbbce28e426f28d1f2a6f1043a1618d5b39863
Oscar FR <oskijob@gmail.com>
2023-11-10 01:31:20 +0100

    initial upload

---
+++ container_init.sh
@@ -0,0 +1,1 @@
+docker run -it  -p 3000:3000 -h 0.0.0.0 -v /Users/cibeles/www/pruebas/dockers/react/node_data:/app/react-app a50950bd57ea
---
+++ node_data/src/components/component1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Component1 extends React.Component {
+
+    render(){
+        return (
+            <React.Fragment>
+                <div>Ejemplo de componente 1</div>
+                <button>Otro elemento</button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Component1;
