e1ca867a1c09e768d5ffd38780df6e4830623000
Oscar FR <oskijob@gmail.com>
2023-11-10 01:54:07 +0100

    delete container_init && add info

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react
+1. Custom path and port (if necessary) 
+2. Execute docker compose up
--- container_init.sh
+++
@@ -1,1 +0,0 @@
-docker run -it  -p 3000:3000 -h 0.0.0.0 -v /Users/cibeles/www/pruebas/dockers/react/node_data:/app/react-app a50950bd57ea
