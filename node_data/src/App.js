4fc4784ce8b6f0dc17c9004c88d91c1d3bfb3402
Oscar FR <oskijob@gmail.com>
2023-11-14 00:31:26 +0100

    add multipage react

--- node_data/src/App.js
+++ node_data/src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import List from './components/Products';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Home from './pages';
 
 function App() {
   return (
+    <>
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+    </Routes>
+</Router>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <List></List>
       </header>
     </div>
+    </>
   );
 }
 
